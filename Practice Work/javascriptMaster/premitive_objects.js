e06953c600d7bf62e88b285b180b97fc57410192
afide596 <afride596@gmail.com>
2023-01-13 02:24:23 +0530

    Learn about objects

---
+++ Practice Work/javascriptMaster/premitive_objects.js
@@ -0,0 +1,36 @@
+// There are 7  premitivedataType in javascript 
+
+// nnssbbu
+
+// null,number,string,symbol boolean, bigint ,undefined 
+
+let a =null ;
+let b=345;
+let c = true;
+let d =BigInt("549")
+//  d =BigInt("549")+BigInt("2")
+let e ="afride"
+let f= Symbol("i am a nice symbol")
+// let g= undefined
+let g // if not defined anything it also take as undefined
+console.log(a,b,c,d,e,f,g)
+console.log(typeof d)
+
+
+// write a javascript program 
+
+
+
+
+
+// object in javascripts 
+// It is key value pair  
+// it is non premitive dataType 
+
+const item={
+    "afride":true,
+    "ashwaq":false,
+    "harish":56,
+    "mahesh":undefined,
+}
+console.log(item["afride"]);
