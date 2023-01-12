4f97c66e3ce471ff155ce510f421c4bf90d9cf83
afide596 <afride596@gmail.com>
2023-01-13 00:26:05 +0530

    minor changes

--- Practice Work/javascript/ternaryOperator.js
+++ Practice Work/javascript/ternaryOperator.js
@@ -1,2 +1,17 @@
+//show user a signout button if he is authenticated
+//otherwise show hime option to login/signup
+
+
+
+// method 1 
+var authenticated =ture;
+if (authenticated){
+    console.log("show signout button");
+}else{
+    console.log("show signout button");
+}
+
+// method 2 
+
 let userlogin=false;
 userlogin? console.log('logout'): console.log('login');
