da776b6e32f68b35109d8e8b93d1fb57e6200c90
afide596 <afride596@gmail.com>
2023-01-24 22:40:19 +0530

    just started strings

---
+++ Practice Work/javascriptMaster/strings13.js
@@ -0,0 +1,12 @@
+let name ="afride"
+console.log(name.length)
+
+let friend ="pradeep"
+console.log(friend[0]);
+
+
+
+// Template Literals 
+let boy1='pramod'
+let boy2='prabhas'
+console.log(`${boy1} is friend of ${boy2}`);
