3daf738a1ac69933c9c6fc929009a8e016dcb95e
afide596 <afride596@gmail.com>
2023-01-26 21:09:32 +0530

    learned array methods

---
+++ Practice Work/javascriptMaster/Arrays/arrayMethod.js
@@ -0,0 +1,45 @@
+// Array Methods
+// -----------------------------------------------------------------
+
+
+
+// 1.toString():
+
+
+// It just convert anydata type to string using toString() methods
+
+// Example:
+// let number =[1,2,3,4]
+// let number =5&5
+// let myNumber=number.toString()
+// // console.log(typeof number)
+// console.log(typeof myNumber)
+
+
+
+// 2.join():
+// It just joined all the array Elements using a separator 
+// Example:
+// let c=number.join("-")
+// console.log(c, typeof c)
+
+
+
+// 3.pop():
+
+// It removes last Element from Array.And update the original array and retured the popped value
+//  Example:
+//  let number =[1,2,3,4]
+//  let myNumber=number.pop()
+//  console.log(number)
+//  console.log(myNumber)
+
+
+// 4.push():It added a new Element at the end of the Array .and modified the original array and return the new array length
+//   Example:
+ let number =[1,2,3,4]
+ let myNumber=number.push(5)
+ console.log(number)
+ console.log(myNumber)
+
+ 5.shift():
