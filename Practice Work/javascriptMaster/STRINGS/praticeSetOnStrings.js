491063e16fa36bbffada441a7544a452c4ccfaf4
afide596 <afride596@gmail.com>
2023-01-26 16:34:47 +0530

    Practice Done

---
+++ Practice Work/javascriptMaster/STRINGS/praticeSetOnStrings.js
@@ -0,0 +1,52 @@
+// 1. What will the following print in the javscript?
+
+// let string="har\""
+// console.log(string.length)
+
+
+// -------------------------------------------------------
+
+// 2.Explore the include,startWith &endWith Function of a string
+
+// include
+
+// const string="javascript is best programming languages"
+// const word='script'
+// console.log(string.includes(word));
+// console.log(`The word "${word}" ${string.includes(word)? 'is ':" isnot" }in the sentence`)
+
+
+// startWith
+
+// const string="javascript is best programming languages"
+// const word='javascript'
+// // console.log(string.startsWith(word));
+// console.log(`The word "${word}" ${string.startsWith(word)? 'is ':" isnot" }started with sentence`)
+
+// endWith
+
+// const string="javascript is best programming languages"
+// const word='languages'
+// // console.log(string.startsWith(word));
+// console.log(`The word "${word}" ${string.endsWith(word)? 'is ':" isnot" }endwith this sentence`)
+
+// ------------------------------------------------------------------------------------
+
+// 3.Write a program to convert a given string to lowercase
+
+// let string='AFRIDE'
+// let myString=string.toLowerCase()
+// console.log(myString)
+
+// -----------------------------------------------------------------------------------------
+
+
+// 4.Extract the amount out of the string "please give Rs 1000"
+// let string="please give Rs 1000"
+// let amount=Number.parseInt(string.slice("please give Rs ".length)) 
+    
+// console.log( typeof amount);
+
+// ---------------------------------------------------------------------------
+// let friend="deepika"
+// console.log(friend);
