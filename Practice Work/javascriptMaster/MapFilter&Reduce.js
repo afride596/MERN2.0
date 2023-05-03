f5b13b594fb6c1a33080ed359b0d2f5fed7c6473
afide596 <afride596@gmail.com>
2023-05-03 23:45:07 +0530

    mapFiltering

---
+++ Practice Work/javascriptMaster/MapFilter&Reduce.js
@@ -0,0 +1,31 @@
+// let arr =[5,6,8]
+//  //Array map method
+// let array = arr.map((value, index)=>{
+//     console.log(value,index)
+//     return value +2
+// })
+// console.log(array);
+
+
+
+
+
+
+
+
+
+
+// //array filter method
+// let arr = [1,2,3,4,5,6,7,8,9,10]
+// let array = arr.filter((value, index)=>{   
+ 
+//     return value > 5
+// })
+// console.log(array);
+
+// Array reduce nethod   
+// let arr = [1,2,3,4,5,6,7,8,9,10] 
+// let array = arr.reduce((previousValue, currentValue)=>{
+//         return previousValue + currentValue
+//     })
+//     console.log(array);
