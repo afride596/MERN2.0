6f6bfdff449d5dbff8f46766a1ff3e56cce8d41d
afride596 <afride596@gmail.com>
2024-08-13 18:56:47 +0530

    progress steps Done

---
+++ Javascript50Projects/02.progressStep/script.js
@@ -0,0 +1,48 @@
+const progress = document.getElementById("progress-bar-1");
+const circle = document.querySelectorAll(".circle");
+const Prev = document.getElementById("prev");
+const next = document.getElementById("next");
+
+let currentActive = 1;
+
+next.addEventListener("click", () => {
+  currentActive++;
+  if (currentActive > circle.length) {
+    currentActive = circle.length;
+  }
+  update();
+  // console.log(currentActive);
+});
+Prev.addEventListener("click", () => {
+  currentActive--;
+  if (currentActive < 1) {
+    currentActive = 1;
+  }
+  update();
+  // console.log(currentActive);
+});
+function update() {
+  circle.forEach((circle, index) => {
+    // console.log(index);
+
+    if (index < currentActive) {
+      circle.classList.add("actie");
+    } else {
+      circle.classList.remove("actie");
+    }
+  });
+  let active = document.querySelectorAll(".actie");
+  console.log(active.length - 1, circle.length - 1);
+
+  progress.style.width = ((active.length - 1) / (circle.length - 1)) * 99 + "%";
+
+  if (currentActive === 1) {
+    Prev.disabled = true;
+  } else {
+    Prev.disabled = false;
+    next.disabled = false;
+  }
+  if (currentActive === circle.length) {
+    next.disabled = true;
+  }
+}
---
+++ Practice Work/ecmaScript/math.js
@@ -0,0 +1,9 @@
+const hundreds=4000000;
+// console.log(hundreds);
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.4)) 4.4 lower value 4.6 topvalue
+// console.log(Math.ceil(4.1))-topvalue
+// console.log(Math.floor(4.9)) -lowervalue 
+// console.log(Math.pow(2,10))
+console.log(M);
